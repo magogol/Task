9ab1a5e3ec841afe52f611666b4b2236dadad1aa
magogol <ulyana20042304@gmail.com>
2023-01-12 03:43:47 +0300

    Commit

--- src/FileData.java
+++ src/FileData.java
@@ -3,49 +3,23 @@ import java.util.Scanner;
 
 public class FileData {
     public static File Inp(String input) throws IOException {
-        BufferedReader br = null;
-        File inputFile = null;
-        Source so = new Encryption(new DataFile(input));
-        Data dat = new DataFile(input);
-        br = new BufferedReader(new FileReader(input));
-        String str;
-        PrintWriter pwin = new PrintWriter(inputFile);
-        while ((str = br.readLine()) != null) {
-            so.writeData(str);
-            so.readData();
-            pwin.println(so.readData());
-        }
-
-        System.out.println("Encoded: ");
-        System.out.println(dat.readData());
-        System.out.println("Decoded: ");
-        System.out.println(so.readData());
-
+        File inputFile = new File(input);
         if(!inputFile.exists()) {
             inputFile.createNewFile();
-            PrintWriter pw = new PrintWriter(inputFile);
+            PrintWriter pwin = new PrintWriter(inputFile);
             Scanner scan = new Scanner(System.in);
             System.out.println("Enter content of file: ");
             while (scan.hasNextLine()) {
                 String s = scan.nextLine();
-                pw.println(s);
+                pwin.println(s);
             }
         }
         return inputFile;
     }
-    public static File Outp(String input) throws IOException {
-        File out = null;
-        if(input.matches("\\w+\\.txt")){
-            out = new File("out.txt");
-        }
-        if(input.matches("\\w+\\.xml")){
-            out = new File("out.xml");
-        }
-        if(input.matches("\\w+\\.json")){
-            out = new File("out.json");
-        }
-        if (!out.exists())
-            out.createNewFile();
-        return out;
+    public static File Outp(String output) throws IOException {
+        File outputFile = new File(output);
+        if (!outputFile.exists())
+            outputFile.createNewFile();
+        return outputFile;
     }
 }
--- src/Task.java
+++ src/Task.java
@@ -3,17 +3,8 @@ import java.util.Scanner;
 
 public class Task {
     public static void main(String[] args) throws IOException{
-        try {
-            System.out.println("Enter name of file(txt, xml or json): ");
-            Scanner scan = new Scanner(System.in);
-            String name = scan.next();
-            File in = FileData.Inp(name);
-            File out = FileData.Outp(name);
-            CountArithmExpression.CountExp(in, out);
-        } catch (IOException e) {
-            System.out.println("Error: " + e);
-        }
+        GUI gui = new GUI();
+        gui.setVisible(true);
     }
 }
-
 
