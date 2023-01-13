7b947ef16113d99809724fc13f3f1f45d99406b1
magogol <ulyana20042304@gmail.com>
2023-01-13 04:28:21 +0300

    Commit

--- src/FileData.java
+++ src/FileData.java
@@ -1,5 +1,8 @@
 import java.io.*;
 import java.util.Scanner;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+import java.util.zip.ZipOutputStream;
 
 public class FileData {
     public static File Inp(String input) throws IOException {
@@ -21,5 +24,61 @@ public class FileData {
         if (!outputFile.exists())
             outputFile.createNewFile();
         return outputFile;
+    }
+    public static void Zip(String file, String zipfile) throws IOException {
+        FileOutputStream fos = new FileOutputStream(zipfile);
+        ZipOutputStream zipOut = new ZipOutputStream(fos);
+        File fileToZip = new File(file);
+        FileInputStream fis = new FileInputStream(fileToZip);
+        ZipEntry zipEntry = new ZipEntry(fileToZip.getName());
+        zipOut.putNextEntry(zipEntry);
+        final byte[] bytes = new byte[1024];
+        int length;
+        while ((length = fis.read(bytes)) >= 0) {
+            zipOut.write(bytes, 0, length);
+        }
+        zipOut.close();
+        fis.close();
+        fos.close();
+    }
+    public static void UnZip(String zipfile, String unzipfile) throws IOException {
+        File destDir = new File(unzipfile);
+        byte[] buffer = new byte[1024];
+        ZipInputStream zis = new ZipInputStream(new FileInputStream(zipfile));
+        ZipEntry zipEntry = zis.getNextEntry();
+        while (zipEntry != null) {
+            File newFile = newFile(destDir, zipEntry);
+            if (zipEntry.isDirectory()) {
+                if (!newFile.isDirectory() && !newFile.mkdirs()) {
+                    throw new IOException("Failed to create directory " + newFile);
+                }
+            } else {
+                File parent = newFile.getParentFile();
+                if (!parent.isDirectory() && !parent.mkdirs()) {
+                    throw new IOException("Failed to create directory " + parent);
+                }
+                FileOutputStream fos = new FileOutputStream(newFile);
+                int len;
+                while ((len = zis.read(buffer)) > 0) {
+                    fos.write(buffer, 0, len);
+                }
+                fos.close();
+            }
+            zipEntry = zis.getNextEntry();
+        }
+        zis.closeEntry();
+        zis.close();
+    }
+    public static File newFile(File destinationDir, ZipEntry zipEntry) throws IOException {
+        File destFile = new File(destinationDir, zipEntry.getName());
+
+        String destDirPath = destinationDir.getCanonicalPath();
+        String destFilePath = destFile.getCanonicalPath();
+
+        if (!destFilePath.startsWith(destDirPath + File.separator)) {
+            throw new IOException("Entry is outside of the target dir: " + zipEntry.getName());
+        }
+
+        return destFile;
     }
 }
--- src/GUI.java
+++ src/GUI.java
@@ -1,16 +1,16 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 
-public class GUI extends JFrame {
+public class GUI extends JFrame{
     private JButton button1 = new JButton("Txt");
     private JButton button2 = new JButton("Xml");
     private JButton button3 = new JButton("Json");
+    // private JButton zipButton = new JButton("Zipping");
     private JLabel label = new JLabel("Input name of file: ");
     private JTextField input = new JTextField("", 7);
 
@@ -30,11 +30,14 @@ public class GUI extends JFrame {
         button1.addActionListener(new Button1EventListener ());
         button2.addActionListener(new Button2EventListener ());
         button3.addActionListener(new Button3EventListener ());
+
         container.add(button1);
         container.add(button2);
         container.add(button3);
     }
     class Button1EventListener implements ActionListener {
+        private String[] file = {"Zip"};
+        private ImageIcon icon = null;
         public void actionPerformed (ActionEvent e){
             String mess = "The content of output txt file:\n\n";
             String mess2 = "The content of input txt file:\n\n";
@@ -56,12 +59,25 @@ public class GUI extends JFrame {
                 }
                 JOptionPane.showMessageDialog(null,mess2,"Input",JOptionPane.PLAIN_MESSAGE);
                 JOptionPane.showMessageDialog(null,mess,"Output",JOptionPane.PLAIN_MESSAGE);
+                Object res = JOptionPane.showInputDialog(null,"Choose:","File", JOptionPane.QUESTION_MESSAGE, icon, file, file[0]);
+                switch(res.toString()){
+                    case "Zip":
+                        FileData.Zip(input.getText() + ".txt", "compressedTxt.zip");
+                        JOptionPane.showMessageDialog(null, "file was zipped");
+                        Object o = JOptionPane.showConfirmDialog(null,"Unzip file?", "unzip?", JOptionPane.YES_NO_OPTION, JOptionPane.ERROR_MESSAGE, icon);
+                        System.out.println(o);
+                        if(o.toString().matches("0"))
+                            FileData.UnZip("compressedTxt.zip", "unzip.txt");
+                        break;
+                }
             } catch(IOException ex) {
                 System.out.println("Error: " + ex);
             }
         }
     }
     class Button2EventListener implements ActionListener {
+        private String[] file = {"Zip"};
+        private ImageIcon icon = null;
         public void actionPerformed (ActionEvent e){
             String mess = "The content of output xml file:\n\n";
             String mess2 = "The content of input xml file:\n\n";
@@ -82,12 +98,24 @@ public class GUI extends JFrame {
                 }
                 JOptionPane.showMessageDialog(null,mess2,"Input",JOptionPane.PLAIN_MESSAGE);
                 JOptionPane.showMessageDialog(null,mess,"Output",JOptionPane.PLAIN_MESSAGE);
+                Object res = JOptionPane.showInputDialog(null,"Choose:","File", JOptionPane.QUESTION_MESSAGE, icon, file, file[0]);
+                switch(res.toString()){
+                    case "Zip":
+                        FileData.Zip(input.getText() + ".xml", "compressedXml.zip");
+                        JOptionPane.showMessageDialog(null, "file was zipped");
+                        Object o = JOptionPane.showConfirmDialog(null,"Unzip file?", "unzip?", JOptionPane.YES_NO_OPTION, JOptionPane.ERROR_MESSAGE, icon);
+                        if(o.toString().matches("0"))
+                            FileData.UnZip("compressedXml.zip", "unzip.xml");
+                        break;
+                }
             } catch(IOException ex) {
                 System.out.println("Error: " + ex);
             }
         }
     }
     class Button3EventListener implements ActionListener {
+        private String[] file = {"Zip"};
+        private ImageIcon icon = null;
         public void actionPerformed (ActionEvent e){
             String mess = "The content of output json file:\n\n";
             String mess2 = "The content of input json file:\n\n";
@@ -109,9 +137,20 @@ public class GUI extends JFrame {
                 }
                 JOptionPane.showMessageDialog(null,mess2,"Input",JOptionPane.PLAIN_MESSAGE);
                 JOptionPane.showMessageDialog(null,mess,"Output",JOptionPane.PLAIN_MESSAGE);
+                Object res = JOptionPane.showInputDialog(null,"Choose:","File", JOptionPane.QUESTION_MESSAGE, icon, file, file[0]);
+                switch(res.toString()){
+                    case "Zip":
+                        FileData.Zip(input.getText() + ".json", "compressedJson.zip");
+                        JOptionPane.showMessageDialog(null, "file was zipped");
+                        Object o = JOptionPane.showConfirmDialog(null,"Unzip file?", "unzip?", JOptionPane.YES_NO_OPTION, JOptionPane.ERROR_MESSAGE, icon);
+                        if(o.toString().matches("0"))
+                            FileData.UnZip("compressedJson.zip", "unzip.json");
+                        break;
+                }
             } catch(IOException ex) {
                 System.out.println("Error: " + ex);
             }
         }
     }
+
 }
